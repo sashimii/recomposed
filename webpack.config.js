64995990dfeb61180d9afa0aeff16ebcff4bdee8
sashimii <stailor@metrolatorsmbp3.torstar.net>
2017-04-26 14:03:31 -0400

    Updated webpack.config for gh-ages

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: path.resolve('dist'),
+    path: path.resolve('./'),
     filename: 'index_bundle.js'
   },
   module: {
